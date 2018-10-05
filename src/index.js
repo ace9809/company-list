13eef2f0f0250cae22265d9a29dc264b28af1c0a
ace9809 <tktmdwns@naver.com>
2018-10-05 21:17:07 +0900

    Refactor: 프로젝트 리덕스 구조 리팩토링

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'react-app-polyfill/ie9'
 import './index.css';
-import App from './App';
+import App from 'containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
