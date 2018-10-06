420f838fddc6387fe1b5ebf91eb4852ad7eb2f5b
ace9809 <tktmdwns@naver.com>
2018-10-07 00:07:21 +0900

    Fix: 액션 코드 수정

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,45 @@
 /**
  * Created by Ace on 2018. 10. 5..
  */
+import axios from 'axios';
+
+function getCompanyListApi(params) {
+  return axios.get(`http://114.207.113.7:8000/company-search/?search`)
+}
+
+export const getCompanyList = (postId) => dispatch => {
+  dispatch({type: 'GET_COMPANY_STARTED'});
+  return getCompanyListApi(postId).then(
+    (response) => {
+      dispatch({
+        type: 'GET_COMPANY_SUCCESS',
+        payload: response
+      })
+    }
+  ).catch(error => {
+    dispatch({
+      type: 'GET_COMPANY_FAILURE',
+      payload: error
+    });
+    throw(error);
+  })
+
+};
+
+const getCompanyStarted = () => ({
+  type: 'GET_COMPANY_STARTED'
+});
+
+const getCompanySuccess = list => ({
+  type: 'GET_COMPANY_SUCCESS',
+  payload: {
+    ...list
+  }
+});
+
+const getCompanyFailure = error => ({
+  type: 'GET_COMPANY_FAILURE',
+  payload: {
+    error
+  }
+});
