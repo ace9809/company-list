9cf286d8d09c080b3669caaf0566a0056d91e4bc
ace9809 <tktmdwns@naver.com>
2018-10-07 03:01:19 +0900

    Feature: SearchBar 컴포넌트 연결

--- src/containers/CardList/index.js
+++ src/containers/CardList/index.js
@@ -4,8 +4,13 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Card from 'components/Card';
+import SearchBar from 'components/SearchBar';
 
 const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const CardListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
@@ -14,20 +19,26 @@ const Wrapper = styled.div`
 
 class CardList extends Component {
 
+  handleChange = (value) => {
+  };
+
   render() {
     console.log('this.props', this.props);
     return (
       <Wrapper>
-        {
-          this.props.companies.map((company, index) => {
-            return (
-              <Card
-                key={index}
-                company={company}
-              />
-            )
-          })
-        }
+        <SearchBar type="text" onSearchTerm={this.handleChange}/>
+        <CardListWrapper>
+          {
+            this.props.companies.map((company, index) => {
+              return (
+                <Card
+                  key={index}
+                  company={company}
+                />
+              )
+            })
+          }
+        </CardListWrapper>
       </Wrapper>
     );
   }
