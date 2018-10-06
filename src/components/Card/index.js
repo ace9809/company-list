bf0dfc24407930d8aa838dd67a0b9d7d1dc7cc2c
ace9809 <tktmdwns@naver.com>
2018-10-07 02:05:58 +0900

    Fix: Card 컴포넌트 디자인 수정

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,39 @@
 /**
  * Created by Ace on 2018. 10. 7..
  */
-/**
- * Created by Ace on 2018. 10. 6..
- */
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  width: 100px;
-  height: 80px;
-  border: 1px solid black;
-  padding: 10px;
+  margin: 10px;
+  width: 250px;
+  height: 200px;
+  border: 1px solid #DCE0DC;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const CardInfoWrapper = styled.div`
+  width: 100%;
+  padding: 20px;
+`;
+
+const TitleWrapper = styled.div`
+  font-weight: bold;
+  font-size: 30px;
+`;
+
+const LocationWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+const CategoryWrapper = styled.div`
+  margin-top: 10px;
+  color: #999;
 `;
 
 
 const Card = (props) => {
-  console.log('props', props);
-  console.log('name', props.company.name)
   const {
     category,
     location,
@@ -25,9 +41,17 @@ const Card = (props) => {
   } = props.company;
   return (
     <Wrapper>
-      {props.name}
-      {location}
-      {category}
+      <CardInfoWrapper>
+        <TitleWrapper>
+          {name}
+        </TitleWrapper>
+        <LocationWrapper>
+          {location}
+        </LocationWrapper>
+        <CategoryWrapper>
+          #{category}
+        </CategoryWrapper>
+      </CardInfoWrapper>
     </Wrapper>
   )
 };
