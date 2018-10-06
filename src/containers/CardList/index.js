3a9893647ac1087b9ddbbbbcd5a81319fbe5e66a
ace9809 <tktmdwns@naver.com>
2018-10-07 01:41:31 +0900

    Feature: CardList 컨테이너 생성

---
+++ src/containers/CardList/index.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Ace on 2018. 10. 7..
+ */
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Card from 'components/Card';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  position: relative;
+  width: 100%;
+  padding: 4%;
+`;
+
+class CardList extends Component {
+
+  render() {
+    console.log('this.props', this.props);
+    return (
+      <Wrapper>
+        {
+          this.props.companies.map((company, index) => {
+            return (
+              <Card
+                key={index}
+                company={company}
+              />
+            )
+          })
+        }
+      </Wrapper>
+    );
+  }
+}
+
+export default CardList;
+
