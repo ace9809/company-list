ea6e81f00712e36ab62b68746f672baec51c5fcf
ace9809 <tktmdwns@naver.com>
2018-10-06 19:18:22 +0900

    feature: reducers 기본 세팅

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import 'react-app-polyfill/ie9'
 import ReduxThunk from 'redux-thunk'
+import reducers from 'reducers';
 import './index.css';
 import App from 'containers/App';
 import * as serviceWorker from './serviceWorker';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by Ace on 2018. 10. 5..
  */
+import { combineReducers } from 'redux';
+import _ from 'lodash';
+
+const rootReducer = combineReducers({
+  companies
+})
+
+function companies(state = [], action) {
+  console.log('state', state);
+}
