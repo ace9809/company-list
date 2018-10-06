2efdbf9eb1d6877412ebc87fb14b2fc321cc8ec4
ace9809 <tktmdwns@naver.com>
2018-10-06 17:18:24 +0900

    Feature: Header 컴포넌트 연결

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from 'components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Header />
       </div>
     );
   }
