7c35ee279141e639e9ee9f4dd1d48d88ca1fd022
ace9809 <tktmdwns@naver.com>
2018-10-07 00:28:57 +0900

    Feature: card 컴포넌트 연결

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Header from 'components/Header';
+import Card from 'components/Card';
+import test from '../test.json';
+
+const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100%;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Wrapper>
         <Header />
-      </div>
+        <Card />
+      </Wrapper>
     );
   }
 }
