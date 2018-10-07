f74fff14c7001e527e1ca5e67802e41838f4c9b5
ace9809 <tktmdwns@naver.com>
2018-10-07 21:03:59 +0900

    Fix: Card 컴포넌트 스타일 변경

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -15,16 +15,18 @@ const Wrapper = styled.div`
 
 const CardInfoWrapper = styled.div`
   width: 100%;
-  padding: 20px;
+  padding: 20px 20px 0 20px;
+  border-bottom: 1px solid #DCE0DC;
 `;
 
 const TitleWrapper = styled.div`
   font-weight: bold;
-  font-size: 30px;
+  font-size: 20px;
 `;
 
 const LocationWrapper = styled.div`
   margin-top: 10px;
+  height: 60px;
 `;
 
 const CategoryWrapper = styled.div`
