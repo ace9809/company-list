ca4b8e35cc736f6165a72190b326c11f03404ae4
ace9809 <tktmdwns@naver.com>
2018-10-08 17:07:09 +0900

    Feature: 하트 카운트 액션 생성자 및 액셕 생성

--- src/actions/index.js
+++ src/actions/index.js
@@ -49,3 +49,47 @@ const getCompanyFailure = error => ({
     error
   }
 });
+
+function updateHeartApi(id, bool) {
+  console.log('id', id);
+  console.log('bool', bool);
+  if (bool) return axios.put(`http://114.207.113.7:8000/company-heart/${id}/?up`)
+  else return axios.put(`http://114.207.113.7:8000/company-heart/${id}/?down`)
+}
+
+export const updateHeart = (id, bool) => dispatch => {
+  dispatch({type: 'UPDATE_HEART_STARTED'});
+  return updateHeartApi(id, bool).then(
+    (response) => {
+      dispatch({
+        type: 'UPDATE_HEART_SUCCESS',
+        payload: response
+      })
+    }
+  ).catch(error => {
+    dispatch({
+      type: 'UPDATE_HEART_FAILURE',
+      payload: error
+    });
+    throw(error);
+  })
+
+};
+
+const updateHeartStarted = () => ({
+  type: 'UPDATE_HEART_STARTED'
+});
+
+const updateHeartSuccess = list => ({
+  type: 'UPDATE_HEART_SUCCESS',
+  payload: {
+    ...list
+  }
+});
+
+const updateHeartFailure = error => ({
+  type: 'UPDATE_HEART_FAILURE',
+  payload: {
+    error
+  }
+});
